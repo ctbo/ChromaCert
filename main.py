3b5220b7e201c33c22a89d9df1db2bb5d7e11495
Harald Bögeholz <harald@boegeholz.org>
2023-10-23 21:42:42 +0200

    Menus and buttons by ChatGPT.

--- main.py
+++ main.py
@@ -7,6 +7,7 @@ import matplotlib.pyplot as plt
 import numpy as np
 
 from PyQt5.QtWidgets import QApplication, QMainWindow, QVBoxLayout, QWidget
+from PyQt5.QtWidgets import QPushButton, QMenuBar, QMenu
 from matplotlib.backends.backend_qt5agg import (
     FigureCanvasQTAgg as FigureCanvas,
     NavigationToolbar2QT as NavigationToolbar
@@ -18,12 +19,12 @@ class GraphApp(QMainWindow):
         super().__init__()
 
         # Create a wheel graph
-        self.G = nx.wheel_graph(7)
+        self.G = nx.wheel_graph(8)
         self.pos = nx.spring_layout(self.G)
         self.selected_nodes = set()
 
         # Setup the main window
-        self.setWindowTitle('Graph GUI')
+        self.setWindowTitle('ChromaCert')
         self.main_widget = QWidget(self)
         self.setCentralWidget(self.main_widget)
         layout = QVBoxLayout(self.main_widget)
@@ -39,6 +40,37 @@ class GraphApp(QMainWindow):
 
         # Connect the canvas click event to our custom function
         self.canvas.mpl_connect('button_press_event', self.on_click)
+
+        # Add Menu
+        self.add_menu()
+
+        # Add Buttons
+        self.add_buttons(layout)
+
+    def add_menu(self):
+        menuBar = self.menuBar()
+        functionsMenu = menuBar.addMenu('Functions')
+
+        action1 = functionsMenu.addAction('Function 1')
+        action1.triggered.connect(self.function1)
+
+        action2 = functionsMenu.addAction('Function 2')
+        action2.triggered.connect(self.function2)
+
+    def add_buttons(self, layout):
+        button1 = QPushButton('Function 1', self)
+        button1.clicked.connect(self.function1)
+        layout.addWidget(button1)
+
+        button2 = QPushButton('Function 2', self)
+        button2.clicked.connect(self.function2)
+        layout.addWidget(button2)
+
+    def function1(self):
+        print("Function 1 executed")
+
+    def function2(self):
+        print("Function 2 executed")
 
     def draw_graph(self):
         self.ax.clear()
